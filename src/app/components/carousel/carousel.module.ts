27a3c0559b1d12ebe56c4f6ee41c80621c876452
dorrin-sot <dorrinsotoudeh123@gmail.com>
2022-07-18 10:38:25 +0430

    Added Carousel

---
+++ src/app/components/carousel/carousel.module.ts
@@ -0,0 +1,9 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {CarouselComponent} from './carousel.component';
+
+@NgModule({
+    declarations: [CarouselComponent],
+    imports: [CommonModule],
+})
+export class CarouselModule {}
