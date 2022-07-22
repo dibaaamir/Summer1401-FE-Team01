2db76ca0d75da9375c3b443173860e789ecc17df
dorrin-sot <dorrinsotoudeh123@gmail.com>
2022-07-22 22:15:06 +0430

    move generateSuggestionGames() into HomeComponent

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,25 +8,25 @@ import {GameImageData} from '../../models/game-image-data';
     styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-    public suggestionGames = generateSuggestionGames();
-}
+    public suggestionGames = this.generateSuggestionGames();
 
-function generateSuggestionGames(): Array<Game> {
-    return new Array(5).fill(0).map(
-        (_, id) =>
-            new Game(
-                id,
-                'UNCHARTED™: Legacy of Thieves Collection',
-                'uncharted',
-                'در مجموعه UNCHARTED: Legacy of Thieves به دنبال میراث خود باشید و نشان خود را روی نقشه بگذارید. داستان‌سرایی هیجان‌انگیز و سینمایی Naughty Dog و بزرگترین مجموعه‌های اکشن بلاک‌باستر این مجموعه نمادین را تجربه کنید.',
-                8.7,
-                new GameImageData(
-                    ['0.webp', '1.png', '2.png', '3.jpeg'].map((v) => `assets/images/games/uncharted/${v}`),
-                    0,
-                    1,
-                    2,
-                    3
+    public generateSuggestionGames(): Array<Game> {
+        return new Array(5).fill(0).map(
+            (_, id) =>
+                new Game(
+                    id,
+                    'UNCHARTED™: Legacy of Thieves Collection',
+                    'uncharted',
+                    'در مجموعه UNCHARTED: Legacy of Thieves به دنبال میراث خود باشید و نشان خود را روی نقشه بگذارید. داستان‌سرایی هیجان‌انگیز و سینمایی Naughty Dog و بزرگترین مجموعه‌های اکشن بلاک‌باستر این مجموعه نمادین را تجربه کنید.',
+                    8.7,
+                    new GameImageData(
+                        ['0.webp', '1.png', '2.png', '3.jpeg'].map((v) => `assets/images/games/uncharted/${v}`),
+                        0,
+                        1,
+                        2,
+                        3
+                    )
                 )
-            )
-    );
+        );
+    }
 }
