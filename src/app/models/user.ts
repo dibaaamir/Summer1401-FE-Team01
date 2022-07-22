deb3a19eb58a9339fc3e4ae0e6c6f63db268dec1
dorrin-sot <dorrinsotoudeh123@gmail.com>
2022-07-22 22:12:06 +0430

    remove comments from user.ts

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,6 +1,6 @@
 export class User {
     public constructor(public username: string, public password: string) {}
-    public purchasedGames: Array<number> = []; // Game ids
-    public watchlist: Array<number> = []; // Game ids
-    public cart: Array<number> = []; // Game ids
+    public purchasedGames: Array<number> = [];
+    public watchlist: Array<number> = [];
+    public cart: Array<number> = [];
 }
