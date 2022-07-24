604cbd19852178e3d346ff9bc8e5f1c092515ca8
dibaaamir <dibaaamir80@gmail.com>
2022-07-24 16:27:03 +0430

    edit header.component.ts

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,8 @@
 import {Component} from '@angular/core';
-import {NavigationEnd, Router} from '@angular/router';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-    public isInHome: boolean = false;
-
-    public constructor(private router: Router) {
-        router.events.subscribe((event) => {
-            if (event instanceof NavigationEnd) this.isInHome = event.urlAfterRedirects === '/';
-        });
-    }
-}
+export class HeaderComponent {}
