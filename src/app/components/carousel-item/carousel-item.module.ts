d41f5f46eed223f989146404369142bbe517ee52
dorrin-sot <dorrinsotoudeh123@gmail.com>
2022-07-24 18:35:06 +0430

    shiny-button component

--- src/app/components/carousel-item/carousel-item.module.ts
+++ src/app/components/carousel-item/carousel-item.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CarouselItemComponent} from './carousel-item.component';
 import {FormsModule} from '@angular/forms';
+import {ShinyButtonComponent} from '../shiny-button/shiny-button.component';
 
 @NgModule({
-    declarations: [CarouselItemComponent],
-    exports: [CarouselItemComponent],
+    declarations: [CarouselItemComponent, ShinyButtonComponent],
+    exports: [CarouselItemComponent, ShinyButtonComponent],
     imports: [CommonModule, FormsModule],
 })
 export class CarouselItemModule {}
--- src/app/components/footer/footer.module.ts
+++ src/app/components/footer/footer.module.ts
@@ -4,10 +4,12 @@ import {CommonModule} from '@angular/common';
 import {FooterComponent} from './footer.component';
 import {RouterModule} from '@angular/router';
 import {AppRoutingModule} from '../../app-routing.module';
+import {CarouselModule} from '../carousel/carousel.module';
+import {CarouselItemModule} from '../carousel-item/carousel-item.module';
 
 @NgModule({
     declarations: [FooterComponent],
     exports: [FooterComponent],
-    imports: [CommonModule, RouterModule, AppRoutingModule],
+    imports: [CommonModule, RouterModule, AppRoutingModule, CarouselModule, CarouselItemModule],
 })
 export class FooterModule {}
