7791a9954550b264ae9f292860460113d18db11a
dibaaamir <dibaaamir80@gmail.com>
2022-07-24 16:15:07 +0430

    convert imgs to webp 2

--- src/app/pages/home/components/categories/categories.component.ts
+++ src/app/pages/home/components/categories/categories.component.ts
@@ -16,17 +16,17 @@ export class CategoriesComponent {
         {
             title: 'Red Dead Redemption II',
             link: 'red-dead-redemption',
-            image: 'assets/images/Red-dead-redemption.jpg',
+            image: 'assets/images/Red-dead-redemption.webp',
         },
         {
             title: 'Last Of Us II',
             link: 'Last-of-us',
-            image: 'assets/images/last-of-us.png',
+            image: 'assets/images/last-of-us.webp',
         },
         {
             title: 'God Of War',
             link: 'God-Of-War',
-            image: 'assets/images/God-of-war.jpg',
+            image: 'assets/images/God-of-war.webp',
         },
         {
             title: 'Hitman III',
