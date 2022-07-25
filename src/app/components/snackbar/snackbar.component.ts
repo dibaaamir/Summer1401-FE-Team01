1441f70a347677554d1420a099408481628af7d0
dorrin-sot <dorrinsotoudeh123@gmail.com>
2022-07-26 00:27:51 +0430

    added snackbar ability to stay open while hover

--- src/app/components/snackbar/snackbar.component.ts
+++ src/app/components/snackbar/snackbar.component.ts
@@ -6,18 +6,32 @@ import {Component} from '@angular/core';
     styleUrls: ['./snackbar.component.scss'],
 })
 export class SnackbarComponent {
-    public text: string = '';
+    public text!: string;
     public borderColor: string | null = null;
     public shown: boolean = false;
+
+    public timeoutId: number | null = null;
 
     public show(text: string, color: string | null = null): void {
         this.text = text;
         this.borderColor = color;
         this.shown = true;
-        setTimeout(() => this.close(), 2000);
+        this.startCloseTimer();
     }
 
     public close(): void {
         this.shown = false;
+        this.stopCloseTimer();
+    }
+
+    public startCloseTimer(): void {
+        this.timeoutId = setTimeout(() => this.close(), 5000);
+    }
+
+    public stopCloseTimer(): void {
+        if (!!this.timeoutId) {
+            clearTimeout(this.timeoutId);
+            this.timeoutId = null;
+        }
     }
 }
