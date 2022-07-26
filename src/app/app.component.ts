afc2e2462af907d8450fe936f38ef2684263481b
dorrin-sot <dorrinsotoudeh123@gmail.com>
2022-07-27 00:43:43 +0430

    remove some trash and add hover color for switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {AfterViewInit, Component, ViewChild} from '@angular/core';
-import {AuthService} from './services/auth.service';
 import {SnackbarComponent} from './components/snackbar/snackbar.component';
 import {SnackbarService} from './services/snackbar.service';
 
@@ -11,41 +10,9 @@ import {SnackbarService} from './services/snackbar.service';
 export class AppComponent implements AfterViewInit {
     @ViewChild('snackbar') public snackbarRef!: SnackbarComponent;
 
-    public constructor(private authService: AuthService, private snackbarService: SnackbarService) {}
+    public constructor(private snackbarService: SnackbarService) {}
 
     public ngAfterViewInit(): void {
         this.snackbarService.initComponent(this.snackbarRef);
-        // setTimeout(() => this.snackbarService.show('سلام'), 4000);
-    }
-
-    public async testSignup(): Promise<void> {
-        console.log(
-            'testSignup',
-            await this.authService.signup({
-                username: 'BijanProgrammer7',
-                email: 'bijaneisapour7@gmail.com',
-                password: '1234',
-            })
-        );
-    }
-
-    public async testLogin(): Promise<void> {
-        console.log(
-            'testLogin',
-            await this.authService.login({
-                username: 'BijanProgrammer7',
-                password: '1234',
-            })
-        );
-    }
-
-    public async testLogout(): Promise<void> {
-        this.authService.logout();
-        // noinspection JSVoidFunctionReturnValueUsed
-        console.log('testLogout', await this.authService.logout());
-    }
-
-    public async testIsLoggedIn(): Promise<void> {
-        console.log('testIsLoggedIn', await this.authService.isLoggedIn());
     }
 }
