ea9d391a38c973289a72b90dd03edf4ef427cab9
dorrin-sot <dorrinsotoudeh123@gmail.com>
2022-07-27 19:00:20 +0430

    added two seperate forms

--- src/app/components/auth/auth.module.ts
+++ src/app/components/auth/auth.module.ts
@@ -4,9 +4,11 @@ import {AuthComponent} from './auth.component';
 import {FormsModule} from '@angular/forms';
 import {CarouselItemModule} from '../carousel-item/carousel-item.module';
 import {SwitchModule} from '../switch/switch.module';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
 
 @NgModule({
-    declarations: [AuthComponent],
+    declarations: [AuthComponent, LoginComponent, RegisterComponent],
     imports: [CommonModule, FormsModule, CarouselItemModule, SwitchModule],
 })
 export class AuthModule {}
