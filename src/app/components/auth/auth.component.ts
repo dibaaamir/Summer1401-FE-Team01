b209a8a07c7ac07c891c875ca7a02a98b94df4a4
dibaaamir <dibaaamir80@gmail.com>
2022-07-27 21:02:42 +0430

    delete trash

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -6,21 +6,5 @@ import {Component} from '@angular/core';
     styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent {
-    public username!: string;
-    public password!: string;
-    public confirm!: string;
-    public email!: string;
-    public firstName!: string;
-    public lastName!: string;
-
     public isInLoginView: boolean = true;
-
-    public resetFields(): void {
-        this.username = '';
-        this.password = '';
-        this.confirm = '';
-        this.email = '';
-        this.firstName = '';
-        this.lastName = '';
-    }
 }
