025aa30c51d44dd640acc6fc47b0885247d52ebb
dibaaamir <dibaaamir80@gmail.com>
2022-07-31 01:19:07 +0430

    input component, some fixes

--- src/app/components/auth/auth.module.ts
+++ src/app/components/auth/auth.module.ts
@@ -6,9 +6,10 @@ import {CarouselItemModule} from '../carousel-item/carousel-item.module';
 import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
 import {LogoModule} from '../logo/logo.module';
+import {InputModule} from '../input/input.module';
 
 @NgModule({
     declarations: [AuthComponent, LoginComponent, RegisterComponent],
-    imports: [CommonModule, FormsModule, CarouselItemModule, LogoModule],
+    imports: [CommonModule, FormsModule, CarouselItemModule, LogoModule, InputModule],
 })
 export class AuthModule {}
