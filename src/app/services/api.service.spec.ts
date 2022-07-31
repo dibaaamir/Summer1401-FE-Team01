8b9ad767c6f58d2c560ea62d622f442ca9d2f2c4
dorrin-sot <dorrinsotoudeh123@gmail.com>
2022-07-31 21:30:28 +0430

    remove tests

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,12 +1,3 @@
-import {TestBed} from '@angular/core/testing';
-
 import {ApiService} from './api.service';
 
-describe('ApiService', () => {
-    let service: ApiService;
-
-    beforeEach(() => {
-        TestBed.configureTestingModule({});
-        service = TestBed.inject(ApiService);
-    });
-});
+describe('ApiService', () => {});
--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,12 +1,3 @@
-import {TestBed} from '@angular/core/testing';
-
 import {AuthService} from './auth.service';
 
-describe('AuthService', () => {
-    let service: AuthService;
-
-    beforeEach(() => {
-        TestBed.configureTestingModule({});
-        service = TestBed.inject(AuthService);
-    });
-});
+describe('AuthService', () => {});
