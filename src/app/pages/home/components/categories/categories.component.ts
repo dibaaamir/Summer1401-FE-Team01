e5677be9777a6e1e4deb5d5c1c3d696c7512536a
dorrin-sot <dorrinsotoudeh123@gmail.com>
2022-07-31 20:29:27 +0430

    categories overlay for price

--- src/app/pages/home/components/categories/categories.component.ts
+++ src/app/pages/home/components/categories/categories.component.ts
@@ -4,6 +4,8 @@ interface Category {
     title: string;
     link: string;
     image: string;
+    price: number;
+    rating: number;
 }
 
 @Component({
@@ -17,21 +19,29 @@ export class CategoriesComponent {
             title: 'Red Dead Redemption II',
             link: 'red-dead-redemption',
             image: 'assets/images/Red-dead-redemption.webp',
+            price: 250_000,
+            rating: 9.8,
         },
         {
             title: 'Last Of Us II',
             link: 'Last-of-us',
             image: 'assets/images/last-of-us.webp',
+            price: 300_000,
+            rating: 8.5,
         },
         {
             title: 'God Of War',
             link: 'God-Of-War',
             image: 'assets/images/God-of-war.webp',
+            price: 450_000,
+            rating: 9.6,
         },
         {
             title: 'Hitman III',
             link: 'hitman-3',
             image: 'assets/images/Hitman-3.webp',
+            price: 600_000,
+            rating: 9.7,
         },
     ];
 }
