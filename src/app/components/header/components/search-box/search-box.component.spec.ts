ec57c9354da700e345f1ea5885e91e9e4a6ea650
dorrin-sot <dorrinsotoudeh123@gmail.com>
2022-08-01 10:47:06 +0430

    search-box.component.spec.ts

--- src/app/components/header/components/search-box/search-box.component.spec.ts
+++ src/app/components/header/components/search-box/search-box.component.spec.ts
@@ -1,3 +1,39 @@
 import {SearchBoxComponent} from './search-box.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-describe('SearchBoxComponent', () => {});
+describe('SearchBoxComponent', () => {
+    let fixture: ComponentFixture<SearchBoxComponent>;
+    let component: SearchBoxComponent;
+    let host: HTMLElement;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [SearchBoxComponent],
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SearchBoxComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+        host = fixture.nativeElement as HTMLElement;
+    });
+
+    it('tests create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('tests submit', () => {
+        const input = host.querySelector('input');
+        expect(input).toBeTruthy();
+
+        component.searchPhrase = 'hello';
+        fixture.detectChanges();
+
+        component.submitSearch(input!);
+        fixture.detectChanges();
+
+        expect(input!.textContent).toBe('');
+        expect(component.searchPhrase).toBe('');
+    });
+});
