809c12c5b9490d315365e5fb363e00e92df1a567
dorrin-sot <dorrinsotoudeh123@gmail.com>
2022-08-01 19:44:14 +0430

    login.component.spec.ts

--- src/app/components/auth/components/login/login.component.spec.ts
+++ src/app/components/auth/components/login/login.component.spec.ts
@@ -1,3 +1,103 @@
 import {LoginComponent} from './login.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {User} from '../../../../models/user.model';
+import {SnackbarService} from '../../../../services/snackbar.service';
+import {SnackbarServiceMock} from '../../../../mocks/snackbar-service.mock';
+import {FetchMock} from '../../../../mocks/fetch.mock';
 
-describe('LoginComponent', () => {});
+describe('LoginComponent', () => {
+    let fixture: ComponentFixture<LoginComponent>;
+    let component: LoginComponent;
+    let host: HTMLElement;
+
+    let fetchMock: FetchMock;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [],
+            imports: [RouterTestingModule],
+            providers: [{provide: SnackbarService, useValue: new SnackbarServiceMock()}],
+        }).compileComponents();
+
+        fetchMock = new FetchMock();
+        spyOn(window, 'fetch').and.callFake(fetchMock.fetch.bind(fetchMock));
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LoginComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+        host = fixture.nativeElement as HTMLElement;
+    });
+
+    it('tests create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('tests user getter - loginWithEmail=default', () => {
+        testLoginWithEmail();
+    });
+
+    it('tests user getter - loginWithEmail=true', () => {
+        component.loginWithEmail = true;
+        testLoginWithEmail(true);
+    });
+
+    it('tests user getter - loginWithEmail=false', () => {
+        component.loginWithEmail = false;
+        testLoginWithEmail(false);
+    });
+
+    it('tests toggleLoginEmail', () => {
+        component.loginWithEmail = true;
+        fixture.detectChanges();
+        component.toggleLoginEmail();
+        testLoginWithEmail(false);
+    });
+
+    for (let loginWithEmail of [false, true]) {
+        it(`tests isSubmitSuccessful - loginWithEmail=${loginWithEmail}`, async () => {
+            component.loginWithEmail = loginWithEmail;
+            fixture.detectChanges();
+
+            const form = {
+                username: 'BijanProgrammer',
+                password: '1234',
+                email: 'bijaneisapour@gmail.com',
+            };
+            testLoginWithEmail(loginWithEmail, form);
+
+            await component.formSubmitHandler();
+            fixture.detectChanges();
+
+            if (loginWithEmail) expect(component.username).toBe('');
+            else expect(component.email).toBe('');
+
+            testLoginWithEmail(loginWithEmail, form);
+        });
+    }
+
+    // [SECTION] Utility Functions
+
+    const testLoginWithEmail = (loginWithEmail: boolean = false, form: Partial<User> = {}): void => {
+        if (!!form) {
+            component.username = form.username as string;
+            component.password = form.password as string;
+            component.email = form.email as string;
+        }
+        fixture.detectChanges();
+
+        expect(component.loginWithEmail).toBe(loginWithEmail);
+
+        const user = component.user;
+        expect(user.password).toBe(form.password);
+        if (loginWithEmail) {
+            expect(user.email).toBe(form.email);
+            expect(user.username).toBeUndefined();
+        } else {
+            expect(user.username).toBe(form.username);
+            expect(user.email).toBeUndefined();
+        }
+    };
+});
