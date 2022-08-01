6b7168e687502c38561f02583d247499cebc687e
dorrin-sot <dorrinsotoudeh123@gmail.com>
2022-08-01 10:21:31 +0430

    button.component.spec.ts

--- src/app/components/button/button.component.spec.ts
+++ src/app/components/button/button.component.spec.ts
@@ -1,3 +1,50 @@
 import {ButtonComponent} from './button.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-describe('ButtonComponent', () => {});
+describe('ButtonComponent', () => {
+    let fixture: ComponentFixture<ButtonComponent>;
+    let component: ButtonComponent;
+    let host: HTMLElement;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ButtonComponent],
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ButtonComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+        host = fixture.nativeElement as HTMLElement;
+    });
+
+    it('tests create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('tests type - default', () => {
+        const button = host.querySelector('button');
+
+        expect(button).toBeTruthy();
+        expect(component.type).toEqual('contained');
+        testType();
+    });
+
+    const types: Array<'text' | 'outline' | 'contained'> = ['text', 'outline', 'contained'];
+    for (const type of types) {
+        it(`tests type - ${type}`, () => {
+            testType(type);
+        });
+    }
+
+    // [SECTION] Utility Functions
+    const testType = (type?: 'text' | 'outline' | 'contained'): void => {
+        const button = host.querySelector('button');
+        if (!!type) {
+            component.type = type;
+            fixture.detectChanges();
+        }
+        expect(button?.className).toEqual(type || 'contained');
+    };
+});
