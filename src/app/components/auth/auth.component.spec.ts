788639d25da177d586eb54e760d9720602aca563
dorrin-sot <dorrinsotoudeh123@gmail.com>
2022-08-02 19:52:05 +0430

    auth.component.spec.ts

--- src/app/components/auth/auth.component.spec.ts
+++ src/app/components/auth/auth.component.spec.ts
@@ -22,4 +22,20 @@ describe('AuthComponent', () => {
     it('tests create', () => {
         expect(component).toBeTruthy();
     });
+
+    it('tests isInLoginView - default', () => {
+        expect(component.isInLoginView).toBeTrue();
+    });
+
+    for (const isInLoginView of [true, false]) {
+        it(`tests toggleIsInLoginView - ${isInLoginView} => ${!isInLoginView}`, () => {
+            component.isInLoginView = isInLoginView;
+            fixture.detectChanges();
+
+            component.toggleIsInLoginView();
+            fixture.detectChanges();
+
+            expect(component.isInLoginView).toBe(!isInLoginView);
+        });
+    }
 });
