92f9afd2c8f769e9f9027ed66f465c922c6c52fb
dorrin-sot <dorrinsotoudeh123@gmail.com>
2022-08-02 14:57:38 +0430

    remove some comment

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,17 +1,11 @@
 import {TestBed} from '@angular/core/testing';
-// import {RouterTestingModule} from '@angular/router/testing';
-// import {SnackbarService} from './snackbar.service';
-// import {SnackbarServiceMock} from '../mocks/snackbar-service.mock';
 import {ApiService} from './api.service';
 
 describe('ApiService', () => {
     let service: ApiService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({
-            // imports: [RouterTestingModule],
-            // providers: [{provide: SnackbarService, useValue: new SnackbarServiceMock()}],
-        });
+        TestBed.configureTestingModule({});
         service = TestBed.inject(ApiService);
     });
 
