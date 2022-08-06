11e408c915f451c34ddf288a44c6f81f7f7ac12a
Diba <dibaaamir80@gmail.com>
2022-08-06 22:35:33 +0430

    make dir input

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -10,19 +10,8 @@ export class InputComponent {
     @Input() public name!: string;
     @Input() public label!: string;
     @Input() public required: boolean = false;
+    @Input() public direction: 'rtl' | 'ltr' = 'rtl';
 
     @Input() public value!: string;
     @Output() public valueChange = new EventEmitter<string>();
-
-    public get dir(): 'rtl' | 'ltr' {
-        switch (this.name) {
-            case 'username':
-            case 'email':
-            case 'password':
-            case 'confirm':
-                return 'ltr';
-            default:
-                return 'rtl';
-        }
-    }
 }
